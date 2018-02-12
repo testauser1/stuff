745ba95a7807623ac3af17c25ef8df9b9936cac2
Алексей <me@alexsywindows.io>
2018-02-12 07:44:51 -0800

    Update kek.py

--- kek.py
+++ kek.py
@@ -1,10 +1,13 @@
 # import line
 
-# First use
+# fIrSt uSe
+
 # pip install vk
 # pip install telebot
 import vk
 import telebot
 
 # config line
-
+vsession = vk.AuthSession('id_app', 'login', 'pass')
+vapi = vk.API(session)
+bot = telebot.TeleBot('')
